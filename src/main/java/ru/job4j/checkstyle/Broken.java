f8209d26a060690e7a90308411fc247c6fd69498
Ravsan <ravsan@bk.ru>
2023-01-13 12:17:33 +0300

    9. Правила Checkstyle [#504981 #380571]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -6,7 +6,10 @@ public class Broken {
     private String surName;
     private String name;
 
-    void echo() {
+    public Broken() {
+    }
+
+    public void echo() {
     }
 
     public void media(Object obj) {
@@ -16,8 +19,5 @@ public class Broken {
     }
 
     public void method(int a, int b, int c, int d, int e, int f, int g) {
-    }
-
-    public void broken() {
     }
 }
